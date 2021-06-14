6dd708d15215b0ee91eb021ae2f01875db4f470c
Alberto Cano Delgado <canoalboroteo@gmail.com>
2021-06-15 01:12:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ texto = input('Introduzca el texto: ')
 
 st = input('Introduzca el titulo imagen: ' ) + '.png'
 
-color = input('Introduzca los colores en formato R,G,B: ')
+print('\nA continuacion, puede elegir los colores siguiendo el formato RGB')
 
-kit.text_to_handwriting(texto,st)
+colorR = input('\033[0;31;40mColor rojo: ')
+
+colorG = input('\nColor verde: ')
+
+colorB = input('\nColor azul: ')
+
+#Default color is blue
+if(colorR or colorG or colorB == ''):
+    colorR = 0
+    colorG = 0
+    colorB = 255
+
+kit.text_to_handwriting(texto,st,[colorR, colorG, colorB,])
