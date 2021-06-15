fc15bec27612552dfc7dc1ad7d82d3aefc0cfcdf
Alberto Cano Delgado <canoalboroteo@gmail.com>
2021-06-16 01:56:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,73 @@
+# Para retardar el cierre de la consola
+import time
+
 import pywhatkit as kit
 
+# Texto a transcribir
 texto = input('Introduzca el texto: ')
 
-st = input('\nIntroduzca el titulo imagen: ' ) + '.png'
+# Titulo de la imagen en formato png
+st = input('\nIntroduzca el titulo imagen: ') + '.png'
 
+# Para asegurarnos que introducimos un entero para el rango de colores
 def colores():
     numero = -1
     while numero < 0:
         try:
             numero = int(input())
         except:
-            print('El rango es 0-255')
             pass
     return numero
 
-print('\nA continuacion, puede elegir los colores siguiendo el formato RGB')
+# Aplicacion principal
+def main():
 
-print('\nColor rojo(0-255): ', end="")
+    print('\nA continuacion, puede elegir los colores siguiendo el formato RGB')
 
-colorR = colores()
+    # Color red method
+    def rojo():
 
-print('Color verde(0-255): ', end="")
+        print('\nColor rojo(0-255): ', end="")
 
-colorG = colores()
+        colorR = colores()
 
-print('\nColor azul(0-255): ', end="")
+        if(colorR > 255):
+            print('\nEl rango de valores es 0-255')
+            rojo()
+        return colorR
 
-colorB = colores()
+    # Color green method
+    def verde():
 
-try:
-    kit.text_to_handwriting(texto,st,[colorR, colorG, colorB,])
-except:
-    print('Error al crear la imagen')
+        print('\nColor verde(0-255): ', end="")
+
+        colorG = colores()
+
+        if(colorG > 255):
+            print('\nEl rango de valores es 0-255')
+            verde()
+        return colorG
+
+    # Color blue method
+    def azul():
+
+        print('\nColor azul(0-255): ', end="")
+
+        colorB = colores()
+
+        if(colorB > 255):
+            print('\nEl rango de valores es 0-255')
+            azul()
+        return colorB
+
+    try:
+        kit.text_to_handwriting(texto, st, [rojo(), verde(), azul(), ])
+    except:
+        print('\nError al crear la imagen')
+        #Recursive call
+        main()
+    else:
+        print('\nImagen creada con exito!!!')
+        time.sleep(3)
+
+main()
